9ac7616ba4ab1b3a91747d5c163ff27590c59f5c
Priom Sarkar <priomsarker02@gmail.com>
2025-12-10 01:50:35 +0600

    add sign in activity for user for creating new account

--- app/src/main/java/com/example/myapplication/UserLogin.java
+++ app/src/main/java/com/example/myapplication/UserLogin.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,24 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class UserLogin extends AppCompatActivity {
-
+    private Button btnSignIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_user_login);
+        btnSignIn=findViewById(R.id.btnSignin);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(UserLogin.this, SigninActivityForUser.class);
+                startActivity(i);
+            }
         });
     }
 }
