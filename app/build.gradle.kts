9fcd827227e8b6814421dc7c093817f39c630e1a
Priom Sarkar <priomsarker02@gmail.com>
2025-12-23 00:55:53 +0600

    user dashboard and menu iteam added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
 
 
     implementation(libs.recyclerview)
+    implementation(libs.fragment)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/myapplication/UserDashboard.java
+++ app/src/main/java/com/example/myapplication/UserDashboard.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,12 +15,24 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class UserDashboard extends AppCompatActivity {
 
+    private Button btnViewMenu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_user_dashboard);
+
+        btnViewMenu = findViewById(R.id.btnViewMenu);
+        btnViewMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(UserDashboard.this, MenuIteam.class);
+                startActivity(i);
+            }
+        });
     }
+
     public void logout(View view) {
         FirebaseAuth.getInstance().signOut();
         startActivity(new Intent (getApplicationContext(), UserLogin.class));
