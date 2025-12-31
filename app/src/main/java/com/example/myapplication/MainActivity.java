0aca7d5d30c27dcbce4e1ef2864b21eea57f43a1
Priom Sarkar <priomsarker02@gmail.com>
2026-01-01 01:03:40 +0600

    add splash screen.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,37 +2,29 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-/* Updated: clicking the Admin button opens AdminLogin */
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnAdmin;
-    private Button btnUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnAdmin = findViewById(R.id.btnAdmin);
-        btnUser = findViewById(R.id.btnUser);
-        btnAdmin.setOnClickListener(new View.OnClickListener() {
+        Thread thread = new Thread(new Runnable() {
             @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, AdminLogin.class);
-                startActivity(i);
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                    Intent intent = new Intent(MainActivity.this, UserLogin.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         });
-        btnUser.setOnClickListener(new View.OnClickListener() {;
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, UserLogin.class);
-                startActivity(i);
-            }
-        });
-
+        thread.start();
     }
 }
